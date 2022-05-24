dd2e6da2d642e6b35ac13d907b697c1b4d5c004d
Michael Coffey <coffeyjmichael@icloud.com>
2022-05-24 16:02:21 -0700

    Changed kinematic update order

--- Entropic Controller/Entropic Controller.cpp
+++ Entropic Controller/Entropic Controller.cpp
@@ -52,14 +52,14 @@ void checkbound(vector<particle> particles) { //Sequentially checks all input pa
 }
 
 void kinematictick(particle particle) { //Process a kinematic tick for the particle
-    //Update the velocity based on the acceleration
-    particle.velocity[0] += particle.acceleration[0];
-    particle.velocity[1] += particle.acceleration[1];
-    particle.velocity[2] += particle.acceleration[2];
     //Update the position based on the velocity
     particle.position[0] += particle.velocity[0];
     particle.position[1] += particle.velocity[1];
     particle.position[2] += particle.velocity[2];
+   //Update the velocity based on the acceleration
+    particle.velocity[0] += particle.acceleration[0];
+    particle.velocity[1] += particle.acceleration[1];
+    particle.velocity[2] += particle.acceleration[2];
 }
 
 int menu() {
