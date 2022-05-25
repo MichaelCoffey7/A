f64f6c1c2b8c9a43db91ffb67c22764bed82fa22
Michael Coffey <coffeyjmichael@icloud.com>
2022-05-24 20:53:20 -0700

    Redefined entropic tick and added additonal comments

--- Entropic Controller/Entropic Controller.cpp
+++ Entropic Controller/Entropic Controller.cpp
@@ -9,9 +9,10 @@ using namespace std;
 
 //1 entropic length = 0.2am (attometers)
 //The entropic controller can control entropy within a 3 meter by 3 meter cube
-//1 entropic tick = 200as (attoseconds)
-//The entropic controller can simulate time for 1 hour
-//Every entropic tick (200as) the minimum movement of a particle is one entropic length (0.2am); the minimum speed of a particle is 0.2am/200as or 0.001m/s
+//1 entropic tick = 2as (attoseconds)
+//The entropic controller can simulate time 30 seconds
+//Every entropic tick (2as) the minimum movement of a particle is one entropic length (0.2am); the minimum speed of a particle is 0.2am/2as or 0.1m/s
+//The time for a single electron to orbit a hydrogen atom is 24as or 12 entropic ticks
 
 class particle {
 public:
