12b5e4ded8d84f5bbf41c78119858b9d4643f130
Michael Coffey <coffeyjmichael@icloud.com>
2022-06-08 20:12:33 -0700

    Added vectors for specific particles to increase computational speed for certain computations

--- Entropic Controller/Entropic Controller.cpp
+++ Entropic Controller/Entropic Controller.cpp
@@ -32,6 +32,11 @@ string solutionfile = "File not loaded";
 vector<particle> particles; //Vector of particles for selected (initial) .state file
 vector<particle> particles2; //Vector of particles for final .state file
 vector<particle> inputparticles; //Vector of particles to inject at edges of the cube
+
+vector<particle> electrons; //Vector of electron particles, for calculating electric field
+vector<particle> protons; //Vector of proton particles, for calculating electric field and strong interaction
+vector<particle> neutrons; //Vector of neutron particles, for calculating strong interaction
+
 long long int boundary = 7500000000000000000; //The side length of the entropic controller cube; default value is 3 meter by 3 meter cube
 long long int boundary2 = 7500000000000000000; //The side length of the entropic controller cube; default value is 3 meter by 3 meter cube
 
