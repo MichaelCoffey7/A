10b7d48bb2923c008b958e24e69055e4c126392c
Michael Coffey <coffeyjmichael@icloud.com>
2022-07-03 18:33:34 -0700

    Started Omega transition

--- Entropic Controller/Entropic Controller.cpp
+++ Entropic Controller/Entropic Controller.cpp
@@ -73,7 +73,7 @@ void updatetick() {  //Run one tick of the simulation for the vector of particle
 
 int menu() {
     short input = 0;
-    cout << "Entropic Controller developed by Michael Coffey\n";
+    cout << "Omega developed by STEM Series Scientific Manuals\n";
     cout << "File Loaded: " << filename << "\n";
     cout << "Select an option:\n";
     cout << "0. Exit\n";
