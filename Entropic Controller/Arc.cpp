a76944d0923d42be5333ca83f59f94e0c549ff9a
Michael Coffey <coffeyjmichael@icloud.com>
2022-07-04 08:29:48 -0700

    Arc

--- Entropic Controller/Arc.cpp
+++ Entropic Controller/Arc.cpp
@@ -1,4 +1,4 @@
-// Omega.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Arc.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include <iostream>
 #include <fstream>
