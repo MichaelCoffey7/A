0897dea2ae2cd3d7e8552a651183e087d9a0cef5
Michael Coffey <coffeyjmichael@icloud.com>
2022-07-03 19:01:41 -0700

    Comments

--- Entropic Controller/Omega.cpp
+++ Entropic Controller/Omega.cpp
@@ -15,8 +15,9 @@ using namespace std;
 //The time for a single electron to orbit a hydrogen atom is 24as or 12 entropic ticks and the orbit velocity is 2.18*10^6 m/s
 //Vector max size: 209622091746699450
 
-//This needs an acceleration calculator to make it work. Then I will be able to simulate universes using the simulate time function.
-//Later on, I can add a calculator to use compute solution to compute entropic solutions to state change problems.
+//TO DO:
+//Implement simulate time
+//Imeplement generate solution
 
 class particle {
 public:
