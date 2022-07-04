cfbd14629c8c6bc681fd8ade8fdd92d391b50193
Michael Coffey <coffeyjmichael@icloud.com>
2022-07-03 18:52:06 -0700

    Omega Version 0.1

--- Entropic Controller/Omega.cpp
+++ Entropic Controller/Omega.cpp
@@ -1,4 +1,4 @@
-// Entropic Controller.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Omega.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include <iostream>
 #include <fstream>
